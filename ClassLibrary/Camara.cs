3742e6a870ba89329e6eb07e745b40e8cd21bfaa
CUESTA DELGADO SEBASTIAN FRANCISCO <secuestade@uide.edu.ec>
2018-11-02 20:15:17 -0500

    Started Convex Hull Method Migrated to Solution Refereced Class Library

---
+++ ClassLibrary/Camara.cs
@@ -0,0 +1,87 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassLibrary
+{
+    public class Camara
+    {
+        private VideoCapture capture;
+        public Mat Frame;
+        private ImageBox imgBox1, imgBox2;
+        Image<Gray, byte> Edge, Alt;
+        private int Indice1 = 0;
+        public bool calib = false;
+
+        PreProcess pr = new PreProcess();
+        public Camara()
+        {
+
+        }
+
+        public void Inicializar(int Indice, ImageBox imgBox, ImageBox imgBoxA)
+        {
+
+            Indice1 = Indice;
+            imgBox1 = imgBox;
+            imgBox2 = imgBoxA;
+            Frame = new Mat();
+            capture = new VideoCapture(Indice);
+            capture.SetCaptureProperty(CapProp.FrameHeight, imgBox1.Height);
+            capture.SetCaptureProperty(CapProp.FrameWidth, imgBox1.Width);
+            capture.FlipHorizontal = true;
+            capture.ImageGrabbed += CapturarImagen;
+
+
+        }
+
+        private void CapturarImagen(object sender, EventArgs e)
+        {
+            if (capture != null && capture.Ptr != IntPtr.Zero)
+            {
+                try
+                {
+                    capture.Retrieve(Frame);
+                    Alt = pr.Cont(Frame);
+                    Edge = pr.Procesar(Frame);
+                    imgBox1.Image = Edge;
+                    imgBox2.Image = Alt;
+
+                }
+                catch { }
+            }
+
+        }
+
+        public void start()
+        {
+            
+            if (capture != null)
+            {
+                try
+                {
+                    capture.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        public void stop()
+        {
+            capture.Stop();
+            capture.Dispose();
+            imgBox1.Image = null;
+            imgBox2.Image = null;
+        }
+    }
+}
---
+++ ClassLibrary/PreProcess.cs
@@ -0,0 +1,82 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class PreProcess
+    {
+        #region Variables
+        Image<Gray, byte> img;
+        #region Detecion de Mano
+        Image<Hsv, byte> hsv;
+        //Hsv min = new Hsv(135,88,72);
+        //Hsv max = new Hsv(255, 255, 255);
+        Hsv min = new Hsv(100, 0, 0);
+        Hsv max = new Hsv(255, 100, 100);
+        #endregion
+        #region Convex hull
+        VectorOfVectorOfPointF contornos = new VectorOfVectorOfPointF();
+        PointF[][] c;
+        Mat her = new Mat();
+        MCvScalar s = new MCvScalar(255, 100, 100);
+        PointF[] hull;
+        Point[] pts;
+        int m;
+        #endregion
+        #endregion
+        #region Metodos Principales 
+        public PreProcess()
+        {
+
+        }
+
+        public Image<Gray, byte> Procesar(Mat img2)
+        {
+            hsv = img2.ToImage<Hsv, byte>();
+            img = img2.ToImage<Gray, byte>();
+            Piel();
+            Hull();
+            return img;
+        }
+        #endregion
+        #region Metodos Secundarios 
+        public void Piel()
+        {
+            img = hsv.InRange(min, max);
+
+        }
+
+
+        public Image<Gray, byte> Cont(Mat img2)
+        {
+            return img.Canny(200, 400);
+        }
+
+
+        public void Hull()
+        {
+            CvInvoke.FindContours(img, contornos, her, RetrType.External, ChainApproxMethod.ChainApproxNone);
+            CvInvoke.DrawContours(img, contornos, 1, s);
+            /*c = contornos.ToArrayOfArray();
+            foreach (PointF[] p in c ){
+                hull =CvInvoke.ConvexHull(p, true);
+                m = 0;
+                foreach (PointF p2 in hull){
+                    pts[m] = Point.Round(p2);
+                    m++;
+                }
+                CvInvoke.Polylines(img,pts, true,s);
+            }*/
+
+        }
+        #endregion
+    }
+}
